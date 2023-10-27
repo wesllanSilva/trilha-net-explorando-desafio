1c90046e9e939183216df3557b76b7ed31b01c72
Weslan Silva <62728922+wesllanSilva@users.noreply.github.com>
2023-10-26 22:33:08 -0300

    Update Reserva.cs retirado comentarios

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,15 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
+            //
+           
             if (Suite.Capacidade >= hospedes.Count())
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                //
                 throw new Exception(" A quantidade de hospedes é maior que do que a reserva da suíte");
             }
         }
@@ -35,21 +35,18 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
+            // 
             return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            //
+            
             decimal valor = 0;
             valor = DiasReservados * Suite.ValorDiaria;
             decimal desconto = (valor * 10.0M) / 100.0M;
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
+            // 
             if (DiasReservados >= 10)
             {
                 valor -= desconto ;
